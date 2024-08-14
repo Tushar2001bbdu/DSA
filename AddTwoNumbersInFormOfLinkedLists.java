50e40331c871648472f6d460398ff45c1245f40c
Tushar2001bbdu <tusharkumaargupta032@gmail.com>
2024-08-14 16:09:12 +0530

    third commit

--- AddTwoNumbersInFormOfLinkedLists.java
+++ AddTwoNumbersInFormOfLinkedLists.java
@@ -1,4 +1,4 @@
-You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+/*You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
@@ -25,22 +25,21 @@ Constraints:
 The number of nodes in each linked list is in the range [1, 100].
 0 <= Node.val <= 9
 It is guaranteed that the list represents a number that does not have leading zeros.
+*/
 
 
-Solution:
 
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+ 
+ 
 class Solution {
+    public class ListNode {
+        int val;
+       ListNode next;
+        ListNode() {}
+       ListNode(int val) { this.val = val; }
+       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
    if(l1==null || l2==null){
        return null;
